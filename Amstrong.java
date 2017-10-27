3dcbae352615ff7aa5bcfce8e1777988f44731bd
surendra6879 <surendra6879@gmail.com>
2017-10-27 16:22:52 +0530

    added helloworld

---
+++ Amstrong.java
@@ -0,0 +1,45 @@
+public class Amstrong {
+int x=12;
+{
+	m1();
+	System.out.println("hi");
+}
+Amstrong(){
+	System.out.println("constuctor");
+}
+public static void main(String[]args){
+	Amstrong obj=new Amstrong();
+	System.out.println("main");
+	
+}
+public void m1(){
+	System.out.println(y);
+}
+{
+	System.out.println("second");
+}
+int y=21;
+}
+class Emp extends Amstrong{
+	int i=21;
+	{
+		m2();
+
+		System.out.println("sada");
+	}
+	Emp (){
+		System.out.println("second constructor");
+	}
+	//public static void main(String[]args){
+	//	Emp obj1=new Emp();
+	//	System.out.println(" second main");
+//	}
+	public void m2(){
+		System.out.println(j);
+	}
+	{
+		System.out.println("name");
+	}
+		int j=21;
+		
+	}
